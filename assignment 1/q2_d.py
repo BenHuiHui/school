25027490d2cef279a376606e512ae1aafd26f7d7
BenHuiHui <qinghe1992@gmail.com>
2017-09-16 22:43:47 +0800

    completed

--- assignment 1/q2_d.py
+++ assignment 1/q2_d.py
@@ -3,12 +3,12 @@ import os
 import numpy as np
 
 relative_path = "b"
-weights_filenames = ["test.csv", "w-100-40-4.csv", "w-14*28-4.csv", "w-28*6-4.csv"]
-bias_filenames = ["test-b.csv", "b-100-40-4.csv", "b-14*28-4.csv", "b-28*6-4.csv"]
-output_bias_filenames = ["1.csv", "db-100-40-4.csv", "db-14*28-4.csv", "db-28*6-4.csv"]
-output_weights_filenames = ["2.csv", "dw-100-40-4.csv", "dw-14*28-4.csv", "dw-28*6-4.csv"]
+weights_filenames = ["w-100-40-4.csv", "w-14*28-4.csv", "w-28*6-4.csv"]
+bias_filenames = ["b-100-40-4.csv", "b-14*28-4.csv", "b-28*6-4.csv"]
+output_bias_filenames = ["db-100-40-4.csv", "db-14*28-4.csv", "db-28*6-4.csv"]
+output_weights_filenames = ["dw-100-40-4.csv", "dw-14*28-4.csv", "dw-28*6-4.csv"]
+
 dims_list = [
-    [2, 3],
     [14, 100, 40, 4],
     [14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
      14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
@@ -16,7 +16,6 @@ dims_list = [
     [14, 28, 28, 28, 28, 28, 28, 4],
 ]
 x = [-1, 1, 1, 1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1]
-#x = [0.8, -1]
 y = [0, 0, 0, 1]
 
 
@@ -25,7 +24,7 @@ def read_input(name):
     with open(name) as f:
         reader = csv.reader(f)
         for row in reader:
-            lst.append(np.asarray(row[1:]).astype(np.float64))
+            lst.append(np.asarray(row[1:]).astype(np.float))
     return lst
 
 
@@ -38,9 +37,6 @@ def read_weights(name, dimensions):
     for i in range(len(dimensions) - 1):
         dim_prev = dimensions[i]
         dim_next = dimensions[i + 1]
-
-        if len(input_weights[current_row]) != dim_next:
-            print ""
 
         assert len(input_weights[current_row]) == dim_next
 
@@ -71,13 +67,13 @@ def read_bias(name, dimensions):
 
 
 def calculate_output(curr_weights, curr_bias, curr_x):
-    # Offset input layer does not have z.
+    # Offset input layer that does not have z.
     z_list = [np.array([0])]
     output_list = [curr_x]
 
     for i in range(len(curr_weights)):
-        z = np.dot(np.asarray(output_list[i]).astype(np.float64), np.asarray(curr_weights[i]).astype(np.float64)) \
-            + np.asarray(curr_bias[i]).astype(np.float64)
+        z = np.dot(np.asarray(output_list[i]).astype(np.float), np.asarray(curr_weights[i]).astype(np.float)) \
+            + np.asarray(curr_bias[i]).astype(np.float)
 
         z_list.append(z)
 
@@ -88,9 +84,10 @@ def calculate_output(curr_weights, curr_bias, curr_x):
     return output_list, z_list
 
 
-def calculate_softmax(output):
-    output_e = np.exp(output - np.max(output))
-    return output_e / output_e.sum()
+def calculate_softmax(x):
+    shiftx = x - np.max(x)
+    exps = np.exp(shiftx)
+    return exps / np.sum(exps)
 
 
 def relu_gradient(value):
@@ -105,7 +102,7 @@ def pre_calculate_gradients(dimensions, z_list, y_list, soft_max, current_weight
 
     gradient = []
     for i in range(dimensions[len(dimensions)-1]):
-        g = (soft_max[i] - y_list[i]) * relu_gradient(z_list[len(dimensions)-1][i])
+        g = (soft_max[i] - y_list[i]) # * relu_gradient(z_list[len(dimensions)-1][i])
         gradient.append(g)
 
     gradients.append(gradient)
@@ -116,7 +113,7 @@ def pre_calculate_gradients(dimensions, z_list, y_list, soft_max, current_weight
         for j in range(dimensions[i]):
             g = 0
             for k in range(dimensions[i+1]):
-                g = g + gradients[i+1][k] * relu_gradient(z_list[i][j]) * np.float64(current_weights[i][j][k])
+                g = g + gradients[i+1][k] * relu_gradient(z_list[i][j]) * np.float(current_weights[i][j][k])
 
             layer_gradient.append(g)
 
@@ -169,11 +166,8 @@ def do_work(weights_filename, bias_filename, output_weights_filename, output_bia
 
 
 if __name__ == '__main__':
-    for i in range(len(dims_list)):
+    for index in range(len(dims_list)):
 
-        if i != 3:
-            continue
-
-        do_work(weights_filenames[i], bias_filenames[i],
-                output_weights_filenames[i], output_bias_filenames[i],
-                dims_list[i])
+        do_work(weights_filenames[index], bias_filenames[index],
+                output_weights_filenames[index], output_bias_filenames[index],
+                dims_list[index])
